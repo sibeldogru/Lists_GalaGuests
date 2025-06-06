abe06e29d82ac473e2bbd70ea55bdf2ddbe687d0
sibeldogru <sibeldogru@gmail.com>
2025-06-06 22:06:15 +0300

    Add project files.

---
+++ Lists_GalaGuests/Program.cs
@@ -0,0 +1,20 @@
+﻿
+List<string> galaGuests = new List<string>();
+
+galaGuests.Add("Bülent Ersoy");
+galaGuests.Add("Ajda Pekkan");
+galaGuests.Add("Ebru Gündeş");
+galaGuests.Add("Hadise");
+galaGuests.Add("Hande Yener");
+galaGuests.Add("Tarkan");
+galaGuests.Add("Funda Arar");
+galaGuests.Add("Demet Akalın");
+
+
+Console.WriteLine("**Davetli Listesi**");
+
+int i = 1;
+foreach (var guest in galaGuests)
+{
+    Console.WriteLine(i++ + " - " + guest);
+}
